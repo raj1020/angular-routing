4fd1efb5ef1d36ed198032f2462ed31435c60998
raj1020 <raj.1020@hotmail.com>
2020-03-06 12:53:09 +1100

    used wild card route and redirecting routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import {AppComponent} from './app.component';
 import {DepartmentListComponent} from './department-list/department-list.component';
 import {EmployeeListComponent} from './employee-list/employee-list.component';
-
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: 'departments', component: DepartmentListComponent},
-  {path: 'employees', component: EmployeeListComponent}
+  {path: 'employees', component: EmployeeListComponent},
+  { path: '',   redirectTo: '/departments', pathMatch: 'full' },
+  {path: "**", component: PageNotFoundComponent}
 ];
 
 @NgModule({
@@ -14,5 +17,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [DepartmentListComponent, EmployeeListComponent];
+export const routingComponents = [DepartmentListComponent, EmployeeListComponent, PageNotFoundComponent];
 
