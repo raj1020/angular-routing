b348f93f0a250178906b0392117a03828307fad1
raj1020 <raj.1020@hotmail.com>
2020-03-06 13:50:29 +1100

    displayed the params in the department detail page

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
   template: `
-    <p>
-      department-detail works!
-    </p>
+   <h3>You selected department with name = {{departmentName}} and id = {{departmentId}}</h3>
   `,
   styles: []
 })
 export class DepartmentDetailComponent implements OnInit {
 
-  constructor() { }
+  public departmentId;
+  public departmentName;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    let name = this.route.snapshot.paramMap.get('name');
+    this.departmentId = id;
+    this.departmentName = name;
   }
 
 }
